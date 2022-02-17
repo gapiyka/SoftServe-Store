69089b824a11f4184e6a87aac4c5441878d034e3
gapiyka <50524296+gapiyka@users.noreply.github.com>
2022-02-17 18:05:37 +0200

    full shoping logic
    >cart store
    >adding\removing items to\from cart
    >fixs

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 using UnityEngine.UI;
 public struct ObjectFormat
 {
-    public ObjectFormat(string sName, string fName, int priceInt)
+    public ObjectFormat(string sName, string fName, int priceInt, int cartContInt)
     {
         shortName = sName;
         fullName = fName;
         price = priceInt;
+        cartCount = cartContInt;
     }
 
     public string shortName;
     public string fullName;
     public int price;
+    public int cartCount;
 }
 
 public class MenuController : MonoBehaviour
@@ -21,14 +23,17 @@ public class MenuController : MonoBehaviour
     [SerializeField] private Text priceText;
     [SerializeField] private Text titleText;
     [SerializeField] private GameObject hintsBar;
-    //[SerializeField] private GameObject cartMenu;
+    [SerializeField] private GameObject cartMenu;
     [SerializeField] private GameObject objectMenu;
+    [SerializeField] private GameObject prefabListBlock;
+    [SerializeField] private Transform cartList;
     [SerializeField] private Transform spawnPos;
 
     private const int defaultCounter = 1;
     private ObjectFormat[] objectsArray;
     private GameObject tempObject;
     private int tempCounter;
+    private int openedItemCounter;
 
     public bool IsMenuOpened = false;
 
@@ -37,41 +42,60 @@ public class MenuController : MonoBehaviour
     {
         objectsArray = new ObjectFormat[]
         {
-            new ObjectFormat("bed_1", "Bed 1", 350),
-            new ObjectFormat("bed_2", "Bed 2", 400),
-            new ObjectFormat("bed_3", "Bed 3", 450),
-            new ObjectFormat("torchere_1", "Torchere 1", 70),
-            new ObjectFormat("torchere_2", "Torchere 2", 100),
-            new ObjectFormat("torchere_3", "Torchere 3", 120),
-            new ObjectFormat("torchere_4", "Torchere 4", 150),
-            new ObjectFormat("sofa_1", "Sofa 1", 350),
-            new ObjectFormat("sofa_2", "Sofa 2", 450),
-            new ObjectFormat("sofa_3", "Sofa 3", 400),
-            new ObjectFormat("sofa_4", "Sofa 4", 550),
-            new ObjectFormat("coffee_table_1", "Coffee Table 1", 100),
-            new ObjectFormat("coffee_table_2", "Coffee Table 2", 110),
-            new ObjectFormat("coffee_table_3", "Coffee Table 3", 120),
-            new ObjectFormat("coffee_table_4", "Coffee Table 4", 130),
-            new ObjectFormat("cabinet_1", "Cabinet 1", 260),
-            new ObjectFormat("kitchen_chair_1", "Kitchen Chair 1", 40),
-            new ObjectFormat("kitchen_chair_2", "Kitchen Chair 2", 80),
-            new ObjectFormat("kitchen_table_1", "Kitchen Table 1", 180),
-            new ObjectFormat("kitchen_table_2", "Kitchen Table 2", 190),
-            new ObjectFormat("kitchen_shelf_2", "Kitchen Shelf 1", 90),
-            new ObjectFormat("kitchen_shelf_3", "Kitchen Shelf 2", 100),
-            new ObjectFormat("kitchen_shelf_corner_INV", "Kitchen Shelf Corner", 70),
-            new ObjectFormat("modular_kitchen_table_1", "Modular Kitchen Table 1", 140),
-            new ObjectFormat("modular_kitchen_table_2", "Modular Kitchen Table 2", 160),
-            new ObjectFormat("modular_kitchen_table_3", "Modular Kitchen Table 3", 150),
-            new ObjectFormat("modular_kitchen_table_4", "Modular Kitchen Table 4", 170)
+            new ObjectFormat("bed_1", "Bed 1", 350, 0),
+            new ObjectFormat("bed_2", "Bed 2", 400, 0),
+            new ObjectFormat("bed_3", "Bed 3", 450, 0),
+            new ObjectFormat("torchere_1", "Torchere 1", 70, 0),
+            new ObjectFormat("torchere_2", "Torchere 2", 100, 0),
+            new ObjectFormat("torchere_3", "Torchere 3", 120, 0),
+            new ObjectFormat("torchere_4", "Torchere 4", 150, 0),
+            new ObjectFormat("sofa_1", "Sofa 1", 350, 0),
+            new ObjectFormat("sofa_2", "Sofa 2", 450, 0),
+            new ObjectFormat("sofa_3", "Sofa 3", 400, 0),
+            new ObjectFormat("sofa_4", "Sofa 4", 550, 0),
+            new ObjectFormat("coffee_table_1", "Coffee Table 1", 100, 0),
+            new ObjectFormat("coffee_table_2", "Coffee Table 2", 110, 0),
+            new ObjectFormat("coffee_table_3", "Coffee Table 3", 120, 0),
+            new ObjectFormat("coffee_table_4", "Coffee Table 4", 130, 0),
+            new ObjectFormat("cabinet_1", "Cabinet 1", 260, 0),
+            new ObjectFormat("kitchen_chair_1", "Kitchen Chair 1", 40, 0),
+            new ObjectFormat("kitchen_chair_2", "Kitchen Chair 2", 80, 0),
+            new ObjectFormat("kitchen_table_1", "Kitchen Table 1", 180, 0),
+            new ObjectFormat("kitchen_table_2", "Kitchen Table 2", 190, 0),
+            new ObjectFormat("kitchen_shelf_2", "Kitchen Shelf 1", 90, 0),
+            new ObjectFormat("kitchen_shelf_3", "Kitchen Shelf 2", 100, 0),
+            new ObjectFormat("kitchen_shelf_corner_INV", "Kitchen Shelf Corner", 70, 0),
+            new ObjectFormat("modular_kitchen_table_1", "Modular Kitchen Table 1", 140, 0),
+            new ObjectFormat("modular_kitchen_table_2", "Modular Kitchen Table 2", 160, 0),
+            new ObjectFormat("modular_kitchen_table_3", "Modular Kitchen Table 3", 150, 0),
+            new ObjectFormat("modular_kitchen_table_4", "Modular Kitchen Table 4", 170, 0)
         };
     }
 
     public void OpenCart()
     {
-        //Cursor.lockState = CursorLockMode.Confined;
-        //cartMenu.SetActive(true);
-        //IsMenuOpened = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        cartMenu.SetActive(true);
+        IsMenuOpened = true;
+
+        ClearList();// yeah that bad for optimization, but idk correct sollution now
+        //generate list
+        const int startList = 350;
+        const int distList = 35;
+        int goodsCounter = 0;
+        foreach(ObjectFormat obj in objectsArray)
+        {
+            if(obj.cartCount > 0)
+            {
+                Vector3 blockPos = new Vector3(0, startList - (distList * goodsCounter), 0);
+                GameObject listBlock = Instantiate(prefabListBlock, transform.position + blockPos, Quaternion.identity, cartList);
+                listBlock.transform.GetChild(0).GetComponent<Text>().text = obj.fullName; // change item Name by index 0
+                listBlock.transform.GetChild(1).GetComponent<Text>().text = obj.price + "$"; // change item Price by index 1
+                listBlock.transform.GetChild(2).GetComponent<Text>().text = obj.cartCount.ToString(); // change item Quantity by index 2
+                listBlock.transform.GetChild(3).GetComponent<Text>().text = (obj.cartCount * obj.price) + "$"; // change item Sum by index 3
+                goodsCounter++;
+            }
+        }
     }
 
     public void OpenObjectMenu(Transform objT)
@@ -82,6 +106,7 @@ public class MenuController : MonoBehaviour
         tempCounter = defaultCounter;
         counterText.text = tempCounter.ToString();
 
+        openedItemCounter = 0;
         string searchStr = objT.name.Split(" ")[0];
         foreach (ObjectFormat obj in objectsArray)
         {
@@ -90,6 +115,7 @@ public class MenuController : MonoBehaviour
                 CreateObj(objT.gameObject, obj);
                 break;
             }
+            openedItemCounter++;
         }
     }
 
@@ -117,7 +143,7 @@ public class MenuController : MonoBehaviour
     public void CloseCart()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        //cartMenu.SetActive(false);
+        cartMenu.SetActive(false);
         IsMenuOpened = false;
     }
 
@@ -135,7 +161,18 @@ public class MenuController : MonoBehaviour
 
     public void AddToCart()
     {
-        Debug.Log(tempCounter);
+        objectsArray[openedItemCounter].cartCount += tempCounter;
+    }
+
+    public void BuyCart()
+    {
+        ClearList();
+        for(int i = 0; i < objectsArray.Length; i++)
+        {
+            objectsArray[i].cartCount = 0;
+        }
+        // you recommend: 
+        // "No backend logic for the purchase process should be implemented."
     }
 
     void CreateObj(GameObject gameObj, ObjectFormat objF)
@@ -148,5 +185,14 @@ public class MenuController : MonoBehaviour
         tempObject = Instantiate(gameObj, spawnPos.position, gameObj.transform.rotation);
         priceText.text = objF.price + "$";
         titleText.text = objF.fullName;
+    }
+
+    void ClearList()
+    {
+        int items = cartList.childCount;
+        for(int i = 0; i < items; i++)
+        {
+            Destroy(cartList.GetChild(i).gameObject);
+        }
     }
 }
